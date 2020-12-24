9af8616921244584429ba4b3cc6115b2c1e600fa
danielsarran <daniel.sarran@gmail.com>
2020-12-24 01:58:58 -0800

    Refactor communications.py, Add data_storage.py for future data save/load

--- communication.py
+++ communication.py
@@ -6,37 +6,61 @@ import datetime
 
 
 class Communication:
-    """Creates a log of a single communication to/from a Company."""
+    """Creates log of a single communication to/from a Company."""
 
-    def __init__(self, year, month, day, msg_type, note, status):
-        # contacts = heap - with dates, type, and notes
-        # self._application = obj_application
-        self._contact_type = ('Phone Call Out', 'Phone call in', 'Email sent', 'Email received', 'Face-to-face')
-        self._status_type = ('In Progress', 'Cancelled', 'Failed', 'Done')
-
-        self._date = datetime.datetime(year, month, day)
-        self._type = msg_type
-        self._note = note
+    def __init__(self, yr_mo_day_tuple, interaction, notes, status):
+        """
+        //TODO: Docstring
+        :param yr_mo_day_tuple: 
+        :type yr_mo_day_tuple: 
+        :param interaction: "Call", "Email", or "Meeting"
+        :type interaction: 
+        :param notes: 
+        :type notes: 
+        :param status: "Done", "In Progress", "Cancelled", or "Failed"
+        :type status: 
+        """
+        self._date = datetime.datetime(*yr_mo_day_tuple)
+        self._interaction = interaction
+        self._notes = notes
         self._status = status
 
     def __repr__(self):
-        return f'Communication object: "{self._date.strftime("%x"), self._type, self._note}"'
+        return f'Communication object: "{self._date.strftime("%x"), self._interaction, self._notes}"'
 
     def __str__(self):
-        return f'{self._date.strftime("%x")}  {self._type}  {self._status}  --  {self._note}'
+        return f'{self._date.strftime("%x")}  {self._interaction}  {self._status}  --  "{self._notes}"'
 
-    def set_date(self, year, month, day):
-        self._date = datetime.datetime(year, month, day)
+    def set_date(self, yr_mo_day_tuple):
+        self._date = datetime.datetime(*yr_mo_day_tuple)
 
     def get_date(self):
         return self._date.strftime('%x')
 
     # Set & Get
-    # contact type
-    # status
-    # etc
+    def set_interaction(self, interaction):
+        """Sets interaction to 'Call', 'Email', or 'Meeting'."""
+        self._interaction = interaction
+
+    def get_interaction(self):
+        return self._interaction
+
+    def set_notes(self, notes):
+        """Set notes from communication."""
+        self._notes = notes
+
+    def get_notes(self):
+        """Returns notes from communication."""
+        return self._notes
+
+    def set_status(self, status):
+        """Set status to 'Done', 'In Progress', 'Cancelled' or 'Failed'."""
+        self._status = status
+
+    def get_status(self):
+        return self._status
 
 
 if __name__ == '__main__':
-    comm = Communication(2020, 12, 20, 'Email received', 'Invited to Online Assessment!', 'In Progress')
+    comm = Communication((2020, 12, 20), 0, 'You are invited to Online Assessment!', 0)
     print(comm)
