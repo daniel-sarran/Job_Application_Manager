d0b9bfd3214b94d3a643898da2a26037b0928ea7
danielsarran <daniel.sarran@gmail.com>
2020-12-26 16:20:43 -0800

    Add sample dashboard

--- UserInteractions/dashboard.py
+++ UserInteractions/dashboard.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python
 
 """dashboard.py: The main "jump-off" point for the program."""
+
+# Sample dashboard - run to view in console
+
+print(''.center(60))
+print()
+print('Welcome!'.center(60))
+print('You have applied to 67 jobs - nice work!'.center(60))
+print()
+print('     Most recent applications:'.ljust(60))
+print('       1. 12/20/2020, SWE Intern @ Apple')
+print('       2. 12/21/2020, Software Intern @ Blizzard')
+print('       3. 12/21/2020, Software Intern @ Blizzard')
+print()
+print('     Most recent activities:'.ljust(60))
+print('       1. Apple SWE intern -- 12/20/2020, Received rejection letter - that was fast')
+print('       2. Blizzard Software Intern -- 12/21/2020, Invited to Online Assessment')
+
+
+
+
+
